1cd2f46752dab618ae2b67c665711047fd1d8990
nwebb <nwebb5150@gmail.com>
2021-02-21 22:04:30 +0000

    Completed 1.2b

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,11 +9,15 @@ let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
 let question = "Who was the first American woman in space? ";
+
 let correctAnswer = "Sally Ride";
+
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+
+let candidateAnswers = [];
 
 
 function askForName() {
