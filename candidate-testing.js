503c0bdb52039ab45598d0d94e08a0a9bbf1385b
nwebb <nwebb5150@gmail.com>
2021-02-21 23:55:42 +0000

    complete attempt 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -23,65 +23,67 @@ let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Please enter your name: ");
-  
-
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-for (let i = 0; i < questions.length; i++) { candidateAnswers.push((input.question(questions[i])));
-}
-
+  // TODO 1.2b: Ask candidate the question and assign the response as andidateAnswer //
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push((input.question(questions[i])));
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-let numberCorrectlyAnswered = 0
+  let numCorrect = 0
 
-for (let i = 0; i < correctAnswers.length; i++) { 
-if (candidateAnswers[i].toLowerCase()== correctAnswers[i].toLowerCase()) {
-  numberCorrectlyAnswered++;
-}
-}
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+      numCorrect++;
+    }
+  }
 
 
   let grade;
-  grade = (numberCorrectlyAnswered / questions.length) * 100
+  grade = (numCorrect / questions.length) * 100
+
   if (grade >= 80) {
-    testResults = "Passed";
+    testResults = "PASSED";
   } else {
-    testResults = "Failed"
+    testResults = "FAILED";
+
+    console.log(`\n Candidate Name: ${candidateName}`)
+    for (let i = 0; i < questions.length; i++) {
+      console.log(`${i + 1}) ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
+    }
+
+    console.log(`>>> Overall Grade: ${grade}% (${numCorrect} of ${questions.length} response correct <<<`);
+    console.log(`>>> Status: ${testResults} <<<`);
+
+  
   }
-
-console.log(`\n Candidate Name: ${candidateName}`)
-for (let i = 0; questions.length; i++) {
-  console.log(`${i +1}) ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
-}
-
   return grade;
 }
 
+  function runProgram() {
+    askForName();
+    // TODO 1.1c: Ask for candidate's name //
+    console.log('\n' + `Hello, ${candidateName}!` + '\n');
 
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  console.log('\n' + `Hello, ${candidateName}!` + '\n');
-  
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-}
+    askQuestion();
+    gradeQuiz(this.candidateAnswers);
+  }
 
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+  // Don't write any code below this line //
+  // And don't change these or your program will not run as expected //
+  module.exports = {
+    candidateName: candidateName,
+    question: question,
+    correctAnswer: correctAnswer,
+    candidateAnswer: candidateAnswer,
+    questions: questions,
+    correctAnswers: correctAnswers,
+    candidateAnswers: candidateAnswers,
+    gradeQuiz: gradeQuiz,
+    runProgram: runProgram
+  };
